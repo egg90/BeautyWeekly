1662062288fd64761734406bb4255e4182ec552d
Haohua Li <lihaohua90@gmail.com>
2012-08-16 09:41:07 +0800

    FullPage & Pictures

--- BeautyWeekly/Views/ViewPicturePage.xaml.cs
+++ BeautyWeekly/Views/ViewPicturePage.xaml.cs
@@ -18,8 +18,8 @@ namespace BeautyWeekly.Views
     using System.Windows.Media;
     using System.Windows.Media.Animation;
     using System.Windows.Shapes;
-    using Microsoft.Phone.Controls;
     using BeautyWeekly.Resources;
+    using Microsoft.Phone.Controls;
 
     /// <summary>
     /// ViewPicture Page
