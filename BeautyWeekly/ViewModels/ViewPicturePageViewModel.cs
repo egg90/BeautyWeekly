ec9effc8cca2c4afdd089c111a0c78ddaba28880
Haohua Li <lihaohua90@gmail.com>
2012-08-23 01:53:17 +0800

    ViewPicturePage

--- BeautyWeekly/ViewModels/ViewPicturePageViewModel.cs
+++ BeautyWeekly/ViewModels/ViewPicturePageViewModel.cs
@@ -25,8 +25,30 @@ namespace BeautyWeekly
         /// </summary>
         private List<string> photos = new List<string>
         {
-            "/Pictures/01.png",
-            "/Pictures/02.png",
+            "http://qcimg1.mnsfz.com/pic/qingchun/2012-8-17/1/1.jpg",
+            "http://qcimg1.mnsfz.com/pic/qingchun/2012-8-17/1/10.jpg",
+            "http://imgs.xiuna.com/xiezhen/2012-8-22/1/1.jpg",
+
+            "http://image.hnol.net/c/2012-08/22/20/201208222049111388-1559530.jpg",
+            "http://image.hnol.net/c/2012-08/22/20/20120822204911899-1559530.jpg",
+            "http://image.hnol.net/c/2012-08/22/20/2012082220491187610-1559530.jpg",
+            "http://image.hnol.net/c/2012-08/22/20/2012082220491225211-1559530.jpg",
+            "http://image.hnol.net/c/2012-08/22/20/2012082220491235812-1559530.jpg",
+            "http://image.hnol.net/c/2012-08/22/20/201208222049109761-1559530.jpg",
+            "http://image.hnol.net/c/2012-08/22/20/201208222049119642-1559530.jpg",
+            "http://image.hnol.net/c/2012-08/22/20/201208222049113443-1559530.jpg",
+            "http://image.hnol.net/c/2012-08/22/20/201208222049115944-1559530.jpg",
+            "http://image.hnol.net/c/2012-08/22/20/201208222049118355-1559530.jpg",
+            "http://image.hnol.net/c/2012-08/22/20/2012082220491186-1559530.jpg",
+            "http://image.hnol.net/c/2012-08/22/20/201208222049118117-1559530.jpg",
+            "http://image.hnol.net/c/2012-08/22/20/2012082220491248513-1559530.jpg",
+            "http://image.hnol.net/c/2012-08/22/20/2012082220491217914-1559530.jpg",
+            "http://image.hnol.net/c/2012-08/22/20/2012082220491239815-1559530.jpg",
+            "http://image.hnol.net/c/2012-08/22/20/2012082220491268916-1559530.jpg",
+            "http://image.hnol.net/c/2012-08/22/20/201208222049126417-1559530.jpg",
+            "http://image.hnol.net/c/2012-08/22/20/2012082220491297518-1559530.jpg",
+            "http://image.hnol.net/c/2012-08/22/20/20120822205431931-1559530.jpg",
+            "http://image.hnol.net/c/2012-08/22/20/201208222054322162-1559530.jpg",
         };
 
         #region Initialization and Cleanup
